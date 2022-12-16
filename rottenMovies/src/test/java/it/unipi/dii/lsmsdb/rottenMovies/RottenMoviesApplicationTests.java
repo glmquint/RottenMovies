9ae1228c92d3506e56980eab3445a2a122e8279f
Fabio <fabizen99@gmail.com>
2022-12-16 11:19:56 +0100

    add spring initializr

---
+++ rottenMovies/src/test/java/it/unipi/dii/lsmsdb/rottenMovies/RottenMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.dii.lsmsdb.rottenMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RottenMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
