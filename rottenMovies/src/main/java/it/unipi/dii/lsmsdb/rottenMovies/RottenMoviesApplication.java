9d4f019212bf61b6938735ac4a65b2a9d924d6fd
Guillaume Quint <g.quint@studenti.unipi.it>
2022-12-20 09:14:55 +0100

    add POC pages

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/RottenMoviesApplication.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/RottenMoviesApplication.java
@@ -4,26 +4,61 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.DAOLocator;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces.MovieDAO;
 import it.unipi.dii.lsmsdb.rottenMovies.models.Movie;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication
+import java.util.List;
+
+@SpringBootApplication
+@Controller
 public class RottenMoviesApplication {
 
-	public  static void main(String[] args) throws JsonProcessingException {
-		MovieDAO test = DAOLocator.getMovieDAO();
-		System.out.println(test.searchByTitle("Evidence"));
-		/*for(int i  = 2001; i<2022; ++i) {
-			for (Movie movie : test.searchByYear(i)) {
-				//System.out.println(movie);
-			}
-		}*/
-		//test.searchByYearRange(2000, 2000);
-		for(Movie movie : test.searchByUserRatings(10, false)){
-			System.out.println(movie.lessDataString());
-			System.out.println("=============================");
-		}
-	}
-	/*public static void main(String[] args) {
+	public  static void main(String[] args)  {
 		SpringApplication.run(RottenMoviesApplication.class, args);
-	}*/
+	}
+	@GetMapping("/hello/{id}")
+	public String hello(Model model,
+						@RequestParam(value = "name", defaultValue = "World") String name,
+						@RequestParam(value = "amount", defaultValue = "1") String amount,
+						@PathVariable("projectId") int id) {
+		model.addAttribute("return", String.format("Hello %s!", name));
+		model.addAttribute("id", id);
+		/*List<Movie> movies = new
+		for (int i = 0; i < amount; i++) {
+
+		}*/
+		return "hello";
+	}
+
+	@GetMapping("/")
+	public String index(Model model){
+		return "index";
+	}
+
+	@GetMapping("/explore")
+	public  String explore(Model model){
+		return "explore";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model){
+		return "login";
+	}
+
+	@GetMapping("/movie")
+	public  String movie(Model model){
+		return "movie";
+	}
+
+	@GetMapping("/admin-panel")
+	public  String adminPanel(Model model){
+		return "admin-panel";
+	}
 
 }
