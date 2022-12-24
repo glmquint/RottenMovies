cd98ef338af05a9a5a9124d5b1b7d47264d3eb66
Guillaume Quint <g.quint@studenti.unipi.it>
2022-12-24 13:19:42 +0100

    new separated test class not synchronized with git

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/RottenMoviesApplication.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/RottenMoviesApplication.java
@@ -1,31 +1,13 @@
 package it.unipi.dii.lsmsdb.rottenMovies;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.client.MongoCursor;
-import it.unipi.dii.lsmsdb.rottenMovies.DAO.DAOLocator;
-import it.unipi.dii.lsmsdb.rottenMovies.DAO.base.enums.DataRepositoryEnum;
-import it.unipi.dii.lsmsdb.rottenMovies.DAO.exception.DAOException;
-import it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces.MovieDAO;
-import it.unipi.dii.lsmsdb.rottenMovies.DAO.mongoDB.MovieMongoDB_DAO;
-import it.unipi.dii.lsmsdb.rottenMovies.DTO.MovieDTO;
-import it.unipi.dii.lsmsdb.rottenMovies.models.*;
-import org.bson.Document;
-
-import java.util.ArrayList;
 
 //@SpringBootApplication
 public class RottenMoviesApplication {
 
 	public  static void main(String[] args) throws JsonProcessingException {
-
-		mainGiacomo();
+		PersonalTestClass test = new PersonalTestClass();
+		test.testMain();
 	}
-	public static void mainGiacomo(){
-		System.out.println("My main");
-	}
-	/*public static void main(String[] args) {
-		SpringApplication.run(RottenMoviesApplication.class, args);
-	}*/
 
 }
