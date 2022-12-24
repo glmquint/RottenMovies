3e312453f2ea86490cda43b258cb2caa8b8df14e
giacomo.volpi99 <giacomo.volpi99@gmail.com>
2022-12-24 13:05:39 +0100

    Fix MovieDAO interface with query builder

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/interfaces/MovieDAO.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/interfaces/MovieDAO.java
@@ -13,18 +13,16 @@ import java.util.ArrayList;
  * <interface>MovieDao</interface> interface for the DAO of the movie
  */
 public interface MovieDAO extends AutoCloseable {
-    /*
-    MovieDTO searchByTitle(String title) throws DAOException;
-    MovieDTO searchById(ObjectId id) throws DAOException;
-    ArrayList<MovieDTO> searchByYearRange(int startYear, int endYear) throws DAOException;
-    ArrayList<MovieDTO> searchByTopRatings(int rating, boolean type) throws DAOException;
-    ArrayList<MovieDTO> searchByUserRatings(int rating, boolean type) throws DAOException;
-    Boolean delete(MovieDTO toDelete) throws DAOException;
-
-     */
+    MongoCollection<Document> getCollection() throws DAOException;
+    ArrayList<MovieDTO> executeSearchQuery(int page) throws DAOException;
+    Boolean executeDeleteQuery() throws DAOException;
+    void queryBuildSearchByTitle (String title) throws DAOException;
+    void queryBuildSearchByTitleContains(String title) throws DAOException;
+    void queryBuildSearchById(ObjectId id) throws DAOException;
+    void queryBuildSearchByTopRatings(int rating, boolean type) throws DAOException;
+    void queryBuildsearchByUserRatings(int rating, boolean type) throws DAOException;
     Boolean update(MovieDTO updated) throws DAOException;
     Boolean insert(MovieDTO newOne) throws DAOException;
-    MongoCollection<Document> getCollection();
     //List<Movie> searchByYear(int year) throws DAOException;
 
 }
