9e0c58da48e3e88bbd5911bef83abe582903bcd4
Fabio <fabizen99@gmail.com>
2022-12-29 12:17:37 +0100

    add comment and documentation for DAOExeption

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/exception/DAOException.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/exception/DAOException.java
@@ -1,5 +1,10 @@
 package it.unipi.dii.lsmsdb.rottenMovies.DAO.exception;
 
+/**
+ * @author Fabio
+ * <class>DAOException</class> represent the exeption case in which
+ * a particular method is called with the wrong DAO class
+ */
 public class DAOException extends Exception{
 
     public DAOException(){
