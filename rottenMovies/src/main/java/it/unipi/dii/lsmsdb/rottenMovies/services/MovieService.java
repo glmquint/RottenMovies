29c8bc691f44ab50b7bbbfe72049b2640b8599ec
Guillaume Quint <g.quint@studenti.unipi.it>
2022-12-30 12:22:36 +0100

    implement sort functionality in movie explorer

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/services/MovieService.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/services/MovieService.java
@@ -5,6 +5,7 @@ import it.unipi.dii.lsmsdb.rottenMovies.DAO.base.enums.DataRepositoryEnum;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces.MovieDAO;
 import it.unipi.dii.lsmsdb.rottenMovies.DTO.MovieDTO;
 import it.unipi.dii.lsmsdb.rottenMovies.DTO.PageDTO;
+import it.unipi.dii.lsmsdb.rottenMovies.utils.sortOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,7 +51,23 @@ public class MovieService {
                     );
                 }
             }
-            movie_page.setEntries(moviedao.executeSearchQuery(page));
+            sortOptions sort_opt = null;
+            switch (request.getOrDefault("sort", "top_critic_rating")){
+                case "date":
+                    sort_opt = sortOptions.DATE;
+                    break;
+                case "alphabet":
+                    sort_opt = sortOptions.ALPHABET;
+                    break;
+                case "user_rating":
+                    sort_opt = sortOptions.USER_RATING;
+                    break;
+                case "top_critic_rating":
+                default:
+                    sort_opt = sortOptions.TOP_CRITIC_RATING;
+            }
+            System.out.println("searching with sort_opt: " + sort_opt);
+            movie_page.setEntries(moviedao.executeSearchQuery(page, sort_opt, request.getOrDefault("sortOrder", "off").equals("off")?-1:1));
         } catch (Exception e){
             System.err.println(e.getStackTrace());
         }
