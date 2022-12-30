b3a44d3ca0b39b888098ea0902b6c940f140c980
giacomo.volpi99 <giacomo.volpi99@gmail.com>
2022-12-30 13:02:26 +0100

    Code refactoring in update/reviewMovie in ReviewMongoDB_DAO

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/mongoDB/ReviewMongoDB_DAO.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/mongoDB/ReviewMongoDB_DAO.java
@@ -3,11 +3,10 @@ package it.unipi.dii.lsmsdb.rottenMovies.DAO.mongoDB;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.BasicDBObject;
-import com.mongodb.client.AggregateIterable;
-import com.mongodb.client.FindIterable;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.*;
+import com.mongodb.client.result.UpdateResult;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.base.BaseMongoDAO;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.exception.DAOException;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces.ReviewDAO;
@@ -17,7 +16,6 @@ import it.unipi.dii.lsmsdb.rottenMovies.utils.Constants;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import javax.print.Doc;
 import java.util.*;
 
 import static com.mongodb.client.model.Filters.and;
@@ -41,6 +39,7 @@ public class ReviewMongoDB_DAO extends BaseMongoDAO implements ReviewDAO {
     private BasicDBObject buildMovieReviewField(Review r){
         BasicDBObject reviewMovie = new BasicDBObject();
         reviewMovie.put("critic_name",r.getCriticName());
+        reviewMovie.put("top_critic",r.isTopCritic());
         reviewMovie.put("review_type",r.getReviewType());
         reviewMovie.put("review_score",r.getReviewScore());
         reviewMovie.put("review_date",r.getReviewDate());
@@ -54,56 +53,81 @@ public class ReviewMongoDB_DAO extends BaseMongoDAO implements ReviewDAO {
         reviewMovie.put("review_index",index);
         return reviewMovie;
     }
-    private int calcolateRating(int fresh,int rotten){
-        Double div = (double)fresh/((double) fresh+(double) rotten);
-        return (int) (div*100);
+    private int calculateRating(int fresh, int rotten){
+        Double div = ((double)fresh/((double) fresh+(double) rotten));
+        return (int) ((div*100)+0.5);
     }
-    private String calcolateTopCriticStatus(int critic_rating,int elemIndexOrSum,int critic_fresh,int critic_rotten){
-        return (critic_rating>=60)?((critic_rating>=75 && (elemIndexOrSum)>=80 && (critic_fresh+critic_rotten)>=5)?"Certified Fresh":"Fresh"):"Rotten";
+    private String calculateTopCriticStatus(int critic_rating, int totalReviewCount, int critic_fresh, int critic_rotten){
+        return (critic_rating>=60)?((critic_rating>=75 && (totalReviewCount)>=80 && (critic_fresh+critic_rotten)>=5)?"Certified Fresh":"Fresh"):"Rotten";
     }
 
-    private int updateMovieRating (MongoCollection<Document> movieCollection,Bson movieFilter,boolean user,boolean freshReview){
-        Bson projectfield=Projections.fields(excludeId(),include("user_fresh_count","user_rotten_count","top_critic_rotten_count","top_critic_fresh_count"));
+    private int updateMovieRating (MongoCollection<Document> movieCollection,Bson movieFilter,boolean isTop,boolean isFresh,boolean isNew){
+        Bson projectfield=Projections.fields(excludeId(),include("user_fresh_count","user_rotten_count","top_critic_rotten_count","top_critic_fresh_count","user_status","top_critic_status"));
         Document res = movieCollection.find(movieFilter).projection(projectfield).first();
         int user_fresh=res.getInteger("user_fresh_count");
         int user_rotten=res.getInteger("user_rotten_count");
         int critic_fresh=res.getInteger("top_critic_fresh_count");
         int critic_rotten=res.getInteger("top_critic_rotten_count");
-        int elemIndexOrSum=critic_fresh+critic_rotten+user_fresh+user_rotten;
-
-        if(user){
-            if(freshReview){
+        String user_status=res.getString("user_status");
+        String top_critic_status=res.getString("top_critic_status");
+        int totalReviewCount=critic_fresh+critic_rotten+user_fresh+user_rotten;
+        Bson updates=null;
+        if(!isTop){
+            if(isFresh){
                 user_fresh++;
+                updates=Updates.set("user_fresh_count",user_fresh);
+                if(!isNew) {
+                    user_rotten--;
+                    updates = Updates.combine(updates, Updates.set("user_rotten_count", user_rotten));
+                }
             }
             else {
                 user_rotten++;
+                updates=Updates.set("user_rotten_count",user_rotten);
+                if(!isNew) {
+                    user_fresh--;
+                    updates=Updates.combine(updates,Updates.set("user_fresh_count",user_fresh));
+                }
             }
-            int user_rating=calcolateRating(user_fresh,user_rotten);
+            int user_rating= calculateRating(user_fresh,user_rotten);
             String status=(user_rating>=60)?"Upright":"Spilled";
-            Bson updates = Updates.combine(
-                    Updates.set("user_fresh_count",user_fresh),
-                    Updates.set("user_rotten_count",user_rotten),
-                    Updates.set("user_rating",user_rating),
-                    Updates.set("user_status",status));
-            movieCollection.updateOne(movieFilter,updates);
+            if(!status.equals(user_status)){
+                updates=Updates.combine(updates,Updates.set("user_status",status));
+            }
+            updates = Updates.combine(updates,Updates.set("user_rating",user_rating));
         }
         else { // it's a TopCritic
-            if(freshReview){
+            if(isFresh){
                 critic_fresh++;
+                updates=Updates.set("top_critic_fresh_count",critic_fresh);
+                if(!isNew){
+                    critic_rotten--;
+                    updates=Updates.combine(updates,Updates.set("top_critic_rotten_count",critic_rotten));
+                }
             }
             else {
                 critic_rotten++;
+                updates=Updates.set("top_critic_rotten_count",critic_rotten);
+                if(!isNew) {
+                    critic_fresh--;
+                    updates = Updates.combine(updates,Updates.set("top_critic_fresh_count", critic_fresh));
+                }
             }
-            int critic_rating=calcolateRating(critic_fresh,critic_rotten);
-            String critic_status = calcolateTopCriticStatus(critic_rating,elemIndexOrSum,critic_fresh,critic_rotten);
-            Bson updates = Updates.combine(
-                    Updates.set("top_critic_fresh_count",critic_fresh),
-                    Updates.set("top_critic_rotten_count",critic_rotten),
-                    Updates.set("top_critic_rating",critic_rating),
-                    Updates.set("top_critic_status",critic_status));
-            movieCollection.updateOne(movieFilter,updates);
+            int critic_rating= calculateRating(critic_fresh,critic_rotten);
+            String critic_status = calculateTopCriticStatus(critic_rating,totalReviewCount,critic_fresh,critic_rotten);
+            if(!top_critic_status.equals(critic_status)){
+                updates=Updates.combine(updates,Updates.set("top_critic_status",critic_status));
+            }
+            updates = Updates.combine(updates, Updates.set("top_critic_rating",critic_rating));
         }
-        return elemIndexOrSum;
+        try{
+            UpdateResult result=movieCollection.updateOne(movieFilter,updates,new UpdateOptions().upsert(true));
+            System.out.println("Modified document count: " + result.getModifiedCount());
+        }
+        catch (MongoException me) {
+            System.err.println("Unable to update due to an error: " + me);
+        }
+        return totalReviewCount;
     }
     @Override
     public boolean reviewMovie(BaseUser usr, Review review) {
@@ -115,23 +139,24 @@ public class ReviewMongoDB_DAO extends BaseMongoDAO implements ReviewDAO {
         MongoCollection<Document> movieCollection = getMovieCollection();
         MongoCollection<Document> userCollection = getUserCollection();
 
-        boolean freshReview = (review.getReviewType()=="Fresh");
+        boolean freshReview = (review.getReviewType().equals("Fresh"));
         BasicDBObject reviewLast3 = buildLast3ReviewField(review);
         Bson movieFilter = eq("_id", review.getMovie_id());
 
-        int elemOrSum=updateMovieRating(movieCollection,movieFilter,(usr instanceof User),freshReview);
+        int newReviewIndex=updateMovieRating(movieCollection,movieFilter,(usr instanceof TopCritic),freshReview,true);
         review.setCriticName(usr.getUsername());
         BasicDBObject reviewMovie = buildMovieReviewField(review);
         Bson updateMovie = push("review", reviewMovie);
-        movieCollection.updateOne(movieFilter,updateMovie);
+        movieCollection.updateOne(movieFilter,updateMovie); // TODO: try - catch
         Bson filterUsr = eq("_id", usr.getId());
-        BasicDBObject simplyfiedreview = buildSimplyfiedReview(review,elemOrSum);
-        Bson updateMovieUser = push("reviews", simplyfiedreview);
+        BasicDBObject simplyfiedReview = buildSimplyfiedReview(review,newReviewIndex);
+        Bson updateMovieUser = push("reviews", simplyfiedReview);
         userCollection.updateOne(filterUsr,updateMovieUser);
         Bson updateUsr = push("last_3_reviews", reviewLast3);
         userCollection.updateOne(filterUsr,updateUsr);
         userCollection.updateOne(filterUsr,popFirst("last_3_reviews"));
         return true;
+
     }
 
     @Override
@@ -172,38 +197,44 @@ public class ReviewMongoDB_DAO extends BaseMongoDAO implements ReviewDAO {
         return true;
     }
     public boolean update (BaseUser usr,Review reviewUpdated){
-        MongoCollection<Document> userCollection = getUserCollection();
-        MongoCollection<Document> movieCollection = getMovieCollection();
-
         if(reviewUpdated.getMovie_id()==null || usr.getId()==null){
             System.out.println("ReviewMongoDB_DAO.update[ERROR]:review fields cannot be null values! Check movie_id and user_id");
             return false;
         }
+        MongoCollection<Document> userCollection = getUserCollection();
+        MongoCollection<Document> movieCollection = getMovieCollection();
+
         ArrayList<Review> last3 = usr.getLast3Reviews();
-        boolean found=false;
-        boolean needsRatingUpdate=true;
+        //boolean found=false;
+        boolean needsRatingUpdate=false;
+        HashMap<String,String> changedFields = new HashMap();
         int index=0;
         for(Review r:last3){
             if(r.getMovie_id().equals(reviewUpdated.getMovie_id())){
                 index=last3.indexOf(r);
-                if(r.getReviewType().equals(reviewUpdated.getReviewType()))
-                    needsRatingUpdate=false;
-                found=true;
+                if(!r.getReviewType().equals(reviewUpdated.getReviewType())) {
+                    needsRatingUpdate = true;
+                    changedFields.put(".review_type", reviewUpdated.getReviewType());
+                }
+                if(!r.getReviewScore().equals(reviewUpdated.getReviewScore()))
+                    changedFields.put(".review_score",reviewUpdated.getReviewScore());
+                if(!r.getReviewContent().equals(reviewUpdated.getReviewContent()))
+                    changedFields.put(".review_content",reviewUpdated.getReviewContent());
                 break;
             }
 
         }
-        if (found){
-            Bson updates = Updates.combine(
-                    Updates.set("last_3_reviews."+index+".review_score", reviewUpdated.getReviewScore()),
-                    Updates.set("last_3_reviews."+index+".review_date", reviewUpdated.getReviewDate()),
-                    Updates.set("last_3_reviews."+index+".review_content", reviewUpdated.getReviewContent()));
-            if(needsRatingUpdate)
-                updates=Updates.combine(updates,Updates.set("last_3_reviews."+index+".review_type",reviewUpdated.getReviewType()));
+        Bson updates=null;
+        if (!changedFields.isEmpty()){
+            updates = Updates.set("last_3_reviews."+index+".review_date", reviewUpdated.getReviewDate());
+            for(String k:changedFields.keySet()) {
+                updates=Updates.combine(updates,Updates.set("last_3_reviews."+index+k,changedFields.get(k)));
+            }
             userCollection.updateOne(eq("_id",usr.getId()),updates);
         }
+
         ArrayList<SimplyfiedReview> listrev = usr.getReviews();
-        found=false;
+        boolean found=false;
         for(SimplyfiedReview s:listrev){
             if(s.getMovieID().equals(reviewUpdated.getMovie_id())){
                 index=s.getIndex();
@@ -215,60 +246,37 @@ public class ReviewMongoDB_DAO extends BaseMongoDAO implements ReviewDAO {
             return false;
         }
         Bson movieFilter = eq("_id", reviewUpdated.getMovie_id());
-        if(!needsRatingUpdate){
-            Bson updates = Updates.combine(
-                    Updates.set("review." + index + ".review_score", reviewUpdated.getReviewScore()),
-                    Updates.set("review." + index + ".review_date", reviewUpdated.getReviewDate()),
-                    Updates.set("review." + index + ".review_content", reviewUpdated.getReviewContent()));
-            movieCollection.updateOne(movieFilter, updates);
-            return true;
+        Movie m=null;
+        Document dbquery = movieCollection.find(movieFilter).projection(Projections.slice("review",index,1)).first();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            m = mapper.readValue(dbquery.toJson(), Movie.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
-        else {
+        Review revdb=m.getReviews().get(0);
+        changedFields.clear();
+        if(!revdb.getReviewType().equals(reviewUpdated.getReviewType())) {
+            needsRatingUpdate = true;
+            changedFields.put(".review_type", reviewUpdated.getReviewType());
+        }
+        if(!revdb.getReviewScore().equals(reviewUpdated.getReviewScore()))
+            changedFields.put(".review_score",reviewUpdated.getReviewScore());
+        if(!revdb.getReviewContent().equals(reviewUpdated.getReviewContent()))
+            changedFields.put(".review_content",reviewUpdated.getReviewContent());
 
-            Movie m=null;
-            Document dbquery = movieCollection.find(movieFilter).projection(Projections.slice("review",index,1)).first();
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-                m = mapper.readValue(dbquery.toJson(), Movie.class);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+        updates=null;
+        if(needsRatingUpdate){
+            updateMovieRating(movieCollection,movieFilter,(usr instanceof TopCritic),(reviewUpdated.getReviewType().equals("Fresh")),false);
+        }
+        if(!changedFields.isEmpty()) {
+            updates = Updates.set("review." + index + ".review_date", reviewUpdated.getReviewDate());
+            for (String k : changedFields.keySet()) {
+                updates = Updates.combine(updates, Updates.set("review." + index + k, changedFields.get(k)));
             }
-            Review r=m.getReviews().get(0);
-            Bson updates = Updates.combine(
-                    Updates.set("review." + index + ".review_score", reviewUpdated.getReviewScore()),
-                    Updates.set("review." + index + ".review_date", reviewUpdated.getReviewDate()),
-                    Updates.set("review." + index + ".review_content", reviewUpdated.getReviewContent()));
-
-            if(r.getReviewType()!=reviewUpdated.getReviewType()){
-                int one = (reviewUpdated.getReviewType()=="Rotten")?-1:1;
-                    if(usr instanceof BaseUser){
-                        m.setUser_fresh_count((m.getUser_fresh_count())+one);
-                        m.setUser_rotten_count((m.getUser_rotten_count())-one);
-                        m.setUser_rating(calcolateRating(m.getUser_fresh_count(),m.getUser_rotten_count()));
-                        m.setUser_status(m.getUser_rating()>=60?"Fresh":"Rotten");
-                        updates=Updates.combine(updates,
-                                Updates.set("user_fresh_count",m.getUser_fresh_count()),
-                                Updates.set("user_rotten_count",m.getUser_rotten_count()),
-                                Updates.set("user_rating",m.getUser_rating()),
-                                Updates.set("user_status",m.getUser_status()));
-                    }
-                    else {
-                        m.setTop_critic_fresh_count((m.getTop_critic_fresh_count())+one);
-                        m.setTop_critic_rotten_count((m.getTop_critic_rotten_count())-one);
-                        m.setTop_critic_rating(calcolateRating(m.getTop_critic_fresh_count(),m.getTop_critic_rotten_count()));
-                        int sum=m.getTop_critic_rotten_count()+m.getTop_critic_fresh_count()+m.getUser_fresh_count()+m.getUser_rotten_count();
-                        m.setTop_critic_status(calcolateTopCriticStatus(m.getTop_critic_rating(),sum,m.getTop_critic_fresh_count(),m.getTop_critic_rotten_count()));
-                        updates=Updates.combine(updates,
-                                Updates.set("top_critic_fresh_count",m.getTop_critic_fresh_count()),
-                                Updates.set("top_critic_rotten_count",m.getTop_critic_rotten_count()),
-                                Updates.set("top_critic_rating",m.getTop_critic_rating()),
-                                Updates.set("top_critic_status",m.getTop_critic_status()));
-                    }
-                    updates=Updates.combine(updates,Updates.set("review."+index+".review_type",reviewUpdated.getReviewType()));
-                }
-                movieCollection.updateOne(movieFilter, updates);
-                return true;
-            }
+            movieCollection.updateOne(movieFilter, updates);
+        }
+        return true;
     }
     @Override
     public ArrayList<ReviewFeedDTO> getFeed(BaseUser usr, int page) throws DAOException {
