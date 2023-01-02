a364b519799e939296de6b4f5810743ceb562825
giacomo.volpi99 <giacomo.volpi99@gmail.com>
2023-01-02 19:04:01 +0100

    Add userPopulationByGeneration

--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/interfaces/AdminDAO.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/interfaces/AdminDAO.java
@@ -1,4 +1,9 @@
 package it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces;
 
-public interface AdminDAO {
+import it.unipi.dii.lsmsdb.rottenMovies.DAO.exception.DAOException;
+
+import java.util.ArrayList;
+
+public interface AdminDAO extends AutoCloseable{
+    ArrayList<Integer> userPopulationByGeneration (int start,int offset,int index);
 }
--- rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/mongoDB/AdminMongoDB_DAO.java
+++ rottenMovies/src/main/java/it/unipi/dii/lsmsdb/rottenMovies/DAO/mongoDB/AdminMongoDB_DAO.java
@@ -1,7 +1,44 @@
 package it.unipi.dii.lsmsdb.rottenMovies.DAO.mongoDB;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.*;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.base.BaseMongoDAO;
 import it.unipi.dii.lsmsdb.rottenMovies.DAO.interfaces.AdminDAO;
+import it.unipi.dii.lsmsdb.rottenMovies.DTO.GenresLikeDTO;
+import it.unipi.dii.lsmsdb.rottenMovies.utils.Constants;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.exists;
 
 public class AdminMongoDB_DAO extends BaseMongoDAO implements AdminDAO {
+    @Override
+    public ArrayList<Integer> userPopulationByGeneration(int start, int offset,int index) {
+        MongoCollection<Document> collectionUser = getUserCollection();
+        BucketOptions opt = new BucketOptions();
+        ArrayList<Integer> buck=new ArrayList<>();
+        opt.output(new BsonField("population",new Document("$sum",1)));
+        for(int i=0; i<=index; i++){
+            buck.add(start+(offset*i));
+        }
+        AggregateIterable<Document> aggregateResult = collectionUser.aggregate(
+                Arrays.asList(
+                        Aggregates.match(exists("date_of_birth")),
+                        Aggregates.bucket(new Document("$year","$date_of_birth"),buck,opt)
+                )
+        );
+        ArrayList<Integer> resultSet = new ArrayList<>();
+        MongoCursor<Document> cursor = aggregateResult.iterator();
+        while (cursor.hasNext()){
+            Document doc = cursor.next();
+            resultSet.add(doc.getInteger("population"));
+        }
+        return resultSet;
+    }
 }
